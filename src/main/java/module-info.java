646c05dfbf54e8e0c8f0bc90b702088552537ff4
ibnammar-dev <ibnammar.dev@gmail.com>
2025-02-22 01:48:40 +0100

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.ibn.dev.skrtdesktopv2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.ibn.dev.skrtdesktopv2 to javafx.fxml;
+    exports com.ibn.dev.skrtdesktopv2;
+}
